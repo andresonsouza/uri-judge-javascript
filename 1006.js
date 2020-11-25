6f6bd1c3d9ae9e6b949ea9491bb0ad8a01c651e9
Andreson Oliveira de Souza <aosouza@procenge.com.br>
2020-11-25 14:22:55 -0300

    URI Online Judge - 1006

---
+++ 1006.js
@@ -0,0 +1,19 @@
+const { log } = require('console');
+
+var input = require('fs').readFileSync('stdin', 'utf8');
+
+var valores = input.split('\n');
+
+var A = parseFloat(valores.shift());
+var B = parseFloat(valores.shift());
+var C = parseFloat(valores.shift());
+
+var pesoA = 2;
+var pesoB = 3;
+var pesoC = 5;
+var somaDosPesos = 10;
+var casasDecimais = 1;
+
+var media = (((A * pesoA) + (B * pesoB) + (C * pesoC)) / somaDosPesos).toFixed(casasDecimais);
+
+console.log("MEDIA = " + media);
