8bd4670aad13912e501c90b4b6274c573a87c7a1
Andreson Oliveira de Souza <aosouza@procenge.com.br>
2020-11-26 10:27:20 -0300

    feat: URI Online Judge - 1011

---
+++ 1011.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+
+var valores = input.split('\n');
+
+const pi = 3.14159;
+var R = parseFloat(valores.shift());
+
+var volume = (4 / 3.0) * pi * Math.pow(R, 3)
+
+console.log("VOLUME = " + (volume).toFixed(3));
