62667aafbfabc71da8cad7f5272d55925c500517
Andreson Oliveira de Souza <aosouza@procenge.com.br>
2020-12-02 16:23:31 -0300

    URI Online Judge - 1042

---
+++ 1042.js
@@ -0,0 +1,18 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+
+var valores = input.split('\n');
+
+let entrada = valores.shift().split(" ");
+
+var entrada1 = entrada[0];
+var entrada2 = entrada[1];
+var entrada3 = entrada[2];
+
+function ordenarLista(lista) {
+    const listaOrdenada = lista.sort(function (a, b) { return a - b });
+
+    return listaOrdenada;
+}
+
+var exibirLista = ordenarLista(entrada);
+console.log(`${exibirLista[0]}\n${exibirLista[1]}\n${exibirLista[2]}\n\n${entrada1}\n${entrada2}\n${entrada3}`);
